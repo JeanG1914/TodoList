054932662c22621936b0c2fd559b51fe92227653
JeanG1914 <jeanpaulgzea@gmail.com>
2023-01-28 01:58:30 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const List = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos , setTodos] = useState([])
+  return (
+    <div className="container">
+      <h1>To do List</h1>
+      <ul>
+        <li>
+			<input 
+			onChange={(e) => setInputValue(e.target.value)}
+			value={inputValue}
+			onKeyPress ={(e)=> {
+				if (e.key === "Enter"){ setTodos(todos.concat(inputValue));
+			}
+			text="text" placeholder="What else you have to do?">
+			</input>
+		</li>
+        <li>feed the cat <i class="fa-sharp fa-solid fa-trash"></i></li>
+        <li>clean my room <i class="fa-sharp fa-solid fa-trash"></i></li>
+        <li>buy groceries <i class="fa-sharp fa-solid fa-trash"></i></li>
+        <li>wash the car <i class="fa-sharp fa-solid fa-trash"></i></li>
+        <li>ABC <i class="fa-sharp fa-solid fa-trash"></i></li>
+        <li>play video games <i class="fa-sharp fa-solid fa-trash"></i></li>
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default List;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
-import React from "react";
+import React , {useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import List from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<List />, document.querySelector("#app"));
