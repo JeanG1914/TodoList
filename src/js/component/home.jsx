dce6f165ee83094b0616e8a154bf48c6b36b3568
JeanG1914 <jeanpaulgzea@gmail.com>
2023-02-02 01:54:36 +0000

    re-contruction

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,27 +5,24 @@ import React, { useState } from "react";
 //create your first component
 const List = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todos , setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
   return (
     <div className="container">
       <h1>To do List</h1>
       <ul>
         <li>
-			<input 
-			onChange={(e) => setInputValue(e.target.value)}
-			value={inputValue}
-			onKeyPress ={(e)=> {
-				if (e.key === "Enter"){ setTodos(todos.concat(inputValue));
-			}
-			text="text" placeholder="What else you have to do?">
-			</input>
-		</li>
-        <li>feed the cat <i class="fa-sharp fa-solid fa-trash"></i></li>
-        <li>clean my room <i class="fa-sharp fa-solid fa-trash"></i></li>
-        <li>buy groceries <i class="fa-sharp fa-solid fa-trash"></i></li>
-        <li>wash the car <i class="fa-sharp fa-solid fa-trash"></i></li>
-        <li>ABC <i class="fa-sharp fa-solid fa-trash"></i></li>
-        <li>play video games <i class="fa-sharp fa-solid fa-trash"></i></li>
+          <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+              }
+            }}
+            text="text"
+            placeholder="What else you have to do?"
+          ></input>
+        </li>
       </ul>
     </div>
   );
