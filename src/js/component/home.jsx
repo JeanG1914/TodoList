8cd5d5a25dc46ed0e3361bce2bd4b82070ef1480
JeanG1914 <jeanpaulgzea@gmail.com>
2023-02-02 23:08:20 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,22 +6,38 @@ import React, { useState } from "react";
 const List = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [task , setTask] = useState("");
+  const addtask = () => {
+    if (task){
+      let num = task.length+1;
+      let newEntry = {id: num, title: task}
+      setTodos([...todos, newEntry])
+      setTask("")
+    }
+  }
+
   return (
     <div className="container">
       <h1>To do List</h1>
       <ul>
         <li>
           <input
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                setTodos(todos.concat(inputValue));
-              }
-            }}
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
             text="text"
             placeholder="What else you have to do?"
           ></input>
+          <button onClick={addtask}>To do</button>
+          {todos.map((todos,idx)=> {
+            return (
+              <div className="col taskBg" key= {todos.id}>
+                <div>   </div>
+              <span className="index">{idx+1}</span>   
+              <span className="task">{todos.title}</span>
+              </div>
+            )
+          })}
+      
         </li>
       </ul>
     </div>
